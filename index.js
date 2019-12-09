06e4daf3a5fb7e835fbcfa8fd2af67a708fcd718
Diana Carolina Badas <33879699+DianaBaRo@users.noreply.github.com>
2019-12-09 15:17:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 // Code your solution here!
 
 //Print all character of a string
-function printString(myString) {
-    console.log(myString[0]);
-   
-    if (myString.length > 1) {
-      let mySubString = myString.substring(1, myString.length);
-      printString(mySubString);
-    } else {
-      return true;
-    }
-}
 
 function printString(myString) {
   console.log(myString[0]);
